df4a8b18dee4cb6ebbaf5f7ccbd9e6cff07eec25
Meayair <38695183+Meayair@users.noreply.github.com>
2018-12-11 15:04:59 +0800

    Update save.php

--- SERVER/save.php
+++ SERVER/save.php
@@ -1,5 +1,5 @@
 <?php
-include('functions.php');
+include('funcs.php');
 $link = "http://www.meirwo.com/static/images/banner/948.jpg";//输入下载地址
 $BDUSS = "***************************************************************FBQUFBJCQAAAAAAAAAAAEAAABqPHTpTWVheWFpcgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB0MD1wdDA9cbU";//输入BDUSS
 echo bdsave($link,$BDUSS);
